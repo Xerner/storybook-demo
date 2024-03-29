20f09b1e89ba6be52c7c193a4db97978ee0702aa
Kenneth Mead <kennethmead784@gmail.com>
2024-03-29 13:11:59 -0400

    added presentation components

---
+++ src/stories/button.stories.ts
@@ -0,0 +1,53 @@
+import type { Meta, StoryObj } from '@storybook/angular';
+import { fn } from '@storybook/test';
+import { ButtonComponent } from './button.component';
+
+// More on how to set up stories at: https://storybook.js.org/docs/writing-stories
+const meta: Meta<ButtonComponent> = {
+  title: 'Example/Button',
+  component: ButtonComponent,
+  tags: ['autodocs'],
+  argTypes: {
+    backgroundColor: {
+      control: 'color',
+    },
+  },
+  parameters: {
+    layout: "centered"
+  },
+  // Use `fn` to spy on the onClick arg, which will appear in the actions panel once invoked: https://storybook.js.org/docs/essentials/actions#action-args
+  args: { onClick: fn((btn: ButtonComponent) => alert(btn.label + " was clicked!")) },
+};
+
+export default meta;
+type Story = StoryObj<ButtonComponent>;
+
+// More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
+export const Primary: Story = {
+  args: {
+    primary: true,
+    label: 'Primary Button',
+  },
+};
+
+export const Secondary: Story = {
+  args: {
+    label: 'Secondary Button',
+  },
+};
+
+export const Large: Story = {
+  args: {
+    primary: true,
+    size: 'large',
+    label: 'Large Button',
+  },
+};
+
+export const Small: Story = {
+  args: {
+    primary: true,
+    size: 'small',
+    label: 'Small Button',
+  },
+};
